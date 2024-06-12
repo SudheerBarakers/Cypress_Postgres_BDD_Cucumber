15f6e3d267b07318901a3d54b1bb5197e8892ded
Sudheer Baraker <120264688+SudheerBarakers@users.noreply.github.com>
2024-06-12 14:25:38 +0530

    Update stepDefinitions.cy.js

--- cypress/e2e/BDD/Postgresss/Postgresss/stepDefinitions.cy.js
+++ cypress/e2e/BDD/Postgresss/Postgresss/stepDefinitions.cy.js
@@ -6,6 +6,13 @@ Given('Test Database', () => {
     cy.task("SqlQueryPS", "select * from employee").then((res) => {
         console.log(res)
     })
-
-
+When('I do something', ()=>{
+    console.log("When I do something")
+})
+And('I do something', ()=>{
+     console.log("And I do something")
+})
+Then('I do something', ()=>{
+     console.log("Then I do")
+})
 });
